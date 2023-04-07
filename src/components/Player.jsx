6a31ed412b505343114bf33ddc01a13cfd756022
damar <damar@gitlab.lab>
2023-04-07 14:18:06 +0700

    add playback count

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -69,7 +69,12 @@ const Player = () => {
                             </svg>
                             <p>{ info ? info.comment_count : '' }</p>
                         </div>
-
+                        <div className="font-thin flex justify-start align-middle mr-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                            </svg>
+                            <p>{ info ? info.playback_count : '' }</p>
+                        </div>
                     </div>
                     <div className="text-sm font-thin">{ info ? info.user.username : '' }</div>
                 </div>
